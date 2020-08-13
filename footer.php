f729d2f1f07f184d4c1609e38f9af4f5fac711ac
JunjiMuroi <661junji23@gmail.com>
2020-08-14 07:37:42 +0900

    footer.phpを作成しました。

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="wrapper page-footer">
+  
+    <div class="footer-min">
+    <nav>
+      <ul class="footer-nav">
+        <li class="footer-li-home"><a href="index.html">HOME</a></li>
+        <li><a href="service.html">SERVICE</a></li>
+        <li><a href="company.html">COMPANY</a></li>
+        <li><a href="news.html">NEWS</a></li>
+      </ul>
+    </nav>
+    </div>
+    <p class="copy-right"><small>&copy;2020 shonan-hp</small></p>
+  </footer>

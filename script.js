a3b6e51f94c4a7d74e14b260869a6846d6804fe1
isetakuya <tak10031003@icloud.com>
2020-10-11 20:05:44 +0900

    s

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function(){
     $('h1').fadeIn(1000);
     $('h2').fadeIn(3000);
     $(window).on("scroll", function() {
-        $('.mato,.matome,.containerr,.work,.niba').each(function(index, el) {
+        $('.mato,.matome,.containerr,.work,.niba,.main-detail').each(function(index, el) {
             if($(window).scrollTop() > ($(el).offset().top - $(window).height() / 2 )){
                 $(el).addClass('is-over');
             }
